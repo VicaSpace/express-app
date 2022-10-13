85ba89a8751b47beac283a74e412381e976264b0
minhnguyenphamquoc <Minh_Nguyen@mckinsey.com>
2022-10-13 17:01:02 +0700

    feat: add sequelize

--- src/routes/catRoutes.js
+++ src/routes/catRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getFacts } = require('../controllers/cats.js');
+const { getFacts, insertFacts } = require('../controllers/cats.js');
 const { logger } = require('../middlewares/logger.js');
 const router = Router();
 
 router.get('/facts', logger, getFacts);
+
+router.post('/facts', logger, insertFacts);
 
 module.exports = {
   catRouter: router,
